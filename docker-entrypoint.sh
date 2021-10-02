d3db0d3041f9ed90f8c8e66e102baf7034ffe121
Aksel Skogsberg <xskoak@gmail.com>
2021-10-02 18:30:37 +0300

    Added ssl support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,18 @@ then
     sed -E -i "s/port=[0-9].+$/port=$PORT/g" /etc/murmur.ini
 fi
 
+if ! [ -z ${SSLKEY+x} ];
+then
+    sed -E -i "s/\;sslKey=/sslKey=$SSLKEY/g" /etc/murmur.ini
+fi
+
+if ! [ -z ${SSLCERT+x} ];
+then
+    sed -E -i "s/\;sslCert=/sslCert=$SSLCERT/g" /etc/murmur.ini
+fi
+
+
+
 if ! [ -z ${BANDWIDTH+x} ];
 then
     sed -E -i "s/bandwidth=[0-9].+$/bandwidth=$BANDWIDTH/g" /etc/murmur.ini
